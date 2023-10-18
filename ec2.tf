ada37c3d2d6711d38b7d281181ea2e50279bb22b
ankita <ankita@gmail.com>
2023-10-18 23:05:13 +0530

    add

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myinstance" {
-  ami           = "var.ami_id"
-  instance_type = "var.instance_type"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   count         = var.instance_count
 }
